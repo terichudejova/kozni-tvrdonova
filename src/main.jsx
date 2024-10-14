577d4846ea4705ee647ca1421e18027a0c70cc5c
Teri Chudějová <tereza.chudejova@gmail.com>
2024-10-14 13:20:03 +0200

    add react router

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import ServicesAndPrices from './pages/ServicesAndPrices.jsx'
+import Contact from './pages/Contact.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/kozni-tvrdonova/",
+    element: <App/>,
+    children: [
+      {
+        path: "/kozni-tvrdonova/",
+        element: <Home/>
+      },
+      {
+        path: "/kozni-tvrdonova/sluzbyacenik",
+        element: <ServicesAndPrices/>
+      },
+      {
+        path: "/kozni-tvrdonova/kontakt",
+        element: <Contact/>
+      }
+    ]
+  }
+]) 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
---
+++ src/pages/ServicesAndPrices.jsx
@@ -0,0 +1,7 @@
+export default function ServicesAndPrices() {
+    return (
+        <div>
+            <h1>Services and Prices</h1>
+        </div>
+    )
+}
